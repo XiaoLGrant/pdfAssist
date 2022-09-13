e99698b8317bda3792332e175239e423bd9b4fa0
Xiao Grant <xgrant001@gmail.com>
2022-09-13 02:29:14 -0700

    Added layout for generating TX letters.

--- controllers/createTxAlias.js
+++ controllers/createTxAlias.js
@@ -1,12 +1,14 @@
 const TxTemplates = require('../models/TXTemplates')
 const Customers = require('../models/Customers')
+const Users = require('../models/User')
 
 module.exports = {
     getCreateTxAlias: async (req,res)=>{
         try {
             const templateData = await TxTemplates.find().sort({templateType: 1})
             const customers = await Customers.find({userID: req.user.id}).sort({customerName: 1})
-            res.render('createTxAlias.ejs', {templates: templateData, customerList: customers})
+            const userData = await Users.find({_id: req.user.id})
+            res.render('createTxAlias.ejs', {templates: templateData, customerList: customers, user: userData})
         } catch(err) {
             console.log(err)
         } 
